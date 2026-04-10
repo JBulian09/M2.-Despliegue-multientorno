8a46bfb5e3e939dc041f2c313aaa43ca8d1d033d
JBulian09 <A00840442@tec.mx>
2026-04-10 01:03:31 +0000

    new

--- Laboratorio de Multientornos/app.js
+++ Laboratorio de Multientornos/app.js
@@ -1,12 +1,10 @@
-```js
 const config = window.APP_CONFIG;
 document.getElementById("environment").textContent = config.environment;
 document.getElementById("version").textContent = config.version;
 document.getElementById("updatedAt").textContent = config.updatedAt;
 const changesList = document.getElementById("changes");
 config.changes.forEach((change) => {
-const item = document.createElement("li");
-item.textContent = change;
-changesList.appendChild(item);
+  const item = document.createElement("li");
+  item.textContent = change;
+  changesList.appendChild(item);
 });
-`
